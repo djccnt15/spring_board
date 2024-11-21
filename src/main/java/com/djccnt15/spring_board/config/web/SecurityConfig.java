3c3d09f103cdeec3154729e93bb4a471f28fc638
Jay.D <djccnt15@gmail.com>
2024-11-21 22:05:39 +0900

    create temporal Spring Security config

---
+++ src/main/java/com/djccnt15/spring_board/config/web/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.djccnt15.spring_board.config.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    
+    // TODO update temporal security config
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+            (authorizeHttpRequests) -> authorizeHttpRequests
+                .requestMatchers(new AntPathRequestMatcher("/**"))
+                .permitAll()
+        );
+        return http.build();
+    }
+}
