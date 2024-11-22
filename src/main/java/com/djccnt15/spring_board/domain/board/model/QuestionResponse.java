15f32244a388ba9feab75f76d6b33268102431c2
Jay.D <djccnt15@gmail.com>
2024-11-22 21:38:07 +0900

    create QuestionResponse DTO

---
+++ src/main/java/com/djccnt15/spring_board/domain/board/model/QuestionResponse.java
@@ -0,0 +1,26 @@
+package com.djccnt15.spring_board.domain.board.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class QuestionResponse {
+    
+    private Long id;
+    
+    private String subject;
+    
+    private String content;
+    
+    private LocalDateTime createDateTime;
+    
+    private List<AnswerResponse> answerList;
+}
