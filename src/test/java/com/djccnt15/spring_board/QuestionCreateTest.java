1e0a91daa256616d56f562841fa54dbe7d6fdfed
Jay.D <djccnt15@gmail.com>
2024-11-28 22:15:55 +0900

    update QuestionCreateTest to input user info

--- src/test/java/com/djccnt15/spring_board/QuestionCreateTest.java
+++ src/test/java/com/djccnt15/spring_board/QuestionCreateTest.java
@@ -1,10 +1,12 @@
 package com.djccnt15.spring_board;
 
+import com.djccnt15.spring_board.db.entity.UserEntity;
 import com.djccnt15.spring_board.domain.board.model.QuestionForm;
 import com.djccnt15.spring_board.domain.board.service.QuestionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class QuestionCreateTest {
@@ -12,8 +14,17 @@ class QuestionCreateTest {
     @Autowired
     private QuestionService questionService;
     
+    @Autowired
+    private PasswordEncoder encoder;
+    
     @Test
     void testJpa() {
+        var user = UserEntity.builder()
+            .username("test")
+            .password(encoder.encode("test"))
+            .email("test@b.com")
+            .build();
+        
         for (int i = 1; i <= 300; i++) {
             var subject = String.format("테스트 데이터입니다:[%03d]", i);
             var content = "내용무";
@@ -21,7 +32,7 @@ class QuestionCreateTest {
                 .subject(subject)
                 .content(content)
                 .build();
-            questionService.create(question);
+            questionService.create(question, user);
         }
     }
 }
