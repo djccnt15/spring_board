25324e0e4ee514f54dc7f868c5eda7631ac9183a
Jay.D <djccnt15@gmail.com>
2024-12-09 22:10:50 +0900

    update HealthApiController use ResponseEntity for return value resolve #1

--- src/main/java/com/djccnt15/spring_board/domain/index/controller/HealthApiController.java
+++ src/main/java/com/djccnt15/spring_board/domain/index/controller/HealthApiController.java
@@ -1,5 +1,6 @@
 package com.djccnt15.spring_board.domain.index.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +14,8 @@ public class HealthApiController {
      * @return 1
      */
     @GetMapping(path = "/health")
-    public int health() {
-        return 1;
+    public ResponseEntity<Integer> health() {
+        return ResponseEntity.ok(1);
     }
     
     /**
@@ -22,7 +23,7 @@ public class HealthApiController {
      * @return "pong"
      */
     @GetMapping(path = "/ping")
-    public String ping() {
-        return "pong";
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("pong");
     }
 }
