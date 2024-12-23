f85206778e2d155b46d52e6ce30ec302167131d3
Jay.D <djccnt15@gmail.com>
2024-12-23 22:28:09 +0900

    update AdminPrivateController to use custom UserSession class instead of principal from spring security

--- src/main/java/com/djccnt15/spring_board/domain/admin/controller/AdminPrivateController.java
+++ src/main/java/com/djccnt15/spring_board/domain/admin/controller/AdminPrivateController.java
@@ -3,14 +3,14 @@ package com.djccnt15.spring_board.domain.admin.controller;
 import com.djccnt15.spring_board.db.entity.enums.UserRoleEnum;
 import com.djccnt15.spring_board.domain.admin.business.AdminBusiness;
 import com.djccnt15.spring_board.domain.admin.model.ManagerRoleRequest;
+import com.djccnt15.spring_board.domain.auth.model.UserSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.security.Principal;
 
 @Slf4j
 @Controller
@@ -24,16 +24,16 @@ public class AdminPrivateController {
     /**
      * redirect to detail admin page
      * @param model inject from spring
-     * @param principal inject from spring-security
+     * @param user inject user session data
      * @return redirection
      */
     @GetMapping
     public String admin(
         Model model,
-        Principal principal
+        @AuthenticationPrincipal UserSession user
     ) {
         model.addAttribute("showAdminLeftNav", true);
-        var userRole = business.getUserRole(principal);
+        var userRole = business.getUserRole(user);
         if (userRole.equals(UserRoleEnum.ADMIN)) {
             return "redirect:/admin/category";
         }
