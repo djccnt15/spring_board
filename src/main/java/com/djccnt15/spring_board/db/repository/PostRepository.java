978ba28217dc024588ff88a7b4ce13b9b3113daa
Jay.D <djccnt15@gmail.com>
2025-01-05 22:51:21 +0900

    create countPostListByCategory method in PostRepository

--- src/main/java/com/djccnt15/spring_board/db/repository/PostRepository.java
+++ src/main/java/com/djccnt15/spring_board/db/repository/PostRepository.java
@@ -74,4 +74,43 @@ public interface PostRepository extends JpaRepository<PostEntity, Long> {
         @Param("page") int page,
         @Param("keyword") String keyword
     );
+    
+    @Query(
+        value = """
+            SELECT COUNT(*)
+            FROM post AS p
+            JOIN category AS c ON p.category_id = c.id
+            JOIN (
+                SELECT
+                    t1.id,
+                    t1.created_datetime,
+                    t1.title,
+                    t1.content,
+                    t1.post_id
+                FROM post_content AS t1
+                JOIN (
+                    SELECT
+                        post_id,
+                        MAX(version) AS max_version
+                    FROM post_content
+                    GROUP BY post_id
+                    ) AS t2 ON t1.post_id = t2.post_id AND t1.version = t2.max_version
+            ) AS pc ON p.id = pc.post_id
+            WHERE
+                p.is_active = TRUE
+                AND (
+                    c.parent_id = :category_id
+                    OR c.id = :category_id
+                )
+                AND (
+                    title LIKE :keyword
+                    OR content LIKE :keyword
+                )
+            """,
+        nativeQuery = true
+    )
+    int countPostListByCategory(
+        @Param("category_id") long categoryId,
+        @Param("keyword") String keyword
+    );
 }
