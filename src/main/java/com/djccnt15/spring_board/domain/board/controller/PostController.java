3d5293fdd0f1909ad85610155d2d5ba7d3c2106e
Jay.D <djccnt15@gmail.com>
2025-01-07 22:56:14 +0900

    create post list view and its controller

---
+++ src/main/java/com/djccnt15/spring_board/domain/board/controller/PostController.java
@@ -0,0 +1,46 @@
+package com.djccnt15.spring_board.domain.board.controller;
+
+import com.djccnt15.spring_board.domain.board.business.PostBusiness;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Slf4j
+@Controller
+@RequestMapping(path = "/board")
+@RequiredArgsConstructor
+public class PostController {
+    
+    private final PostBusiness business;
+    
+    /**
+     * view controller of post list
+     * @param model inject from spring
+     * @param categoryName name of board - main category
+     * @param size size of post list
+     * @param page number of page
+     * @param keyword search keyword
+     * @return post list view
+     */
+    @GetMapping(path = "/{categoryName}")
+    public String getPostList(
+        Model model,
+        @PathVariable(value = "categoryName") String categoryName,
+        @RequestParam(value = "size", defaultValue = "10") int size,
+        @RequestParam(value = "page", defaultValue = "0") int page,
+        @RequestParam(value = "kw", defaultValue = "") String keyword
+    ) {
+        var response = business.getPostList(categoryName, size, page, keyword);
+        model.addAttribute("response", response);
+        model.addAttribute("categoryName", categoryName);
+        model.addAttribute("size", size);
+        model.addAttribute("page", page);
+        model.addAttribute("kw", keyword);
+        return "post-list";
+    }
+}
