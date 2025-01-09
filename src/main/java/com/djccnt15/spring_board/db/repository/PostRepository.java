048878de5469360eff40514cddb62886c596eba8
Jay.D <djccnt15@gmail.com>
2025-01-09 22:31:05 +0900

    optimize post list select query

--- src/main/java/com/djccnt15/spring_board/db/repository/PostRepository.java
+++ src/main/java/com/djccnt15/spring_board/db/repository/PostRepository.java
@@ -32,11 +32,11 @@ public interface PostRepository extends JpaRepository<PostEntity, Long> {
                     t1.content,
                     t1.post_id
                 FROM post_content AS t1
-                JOIN (
-                    SELECT post_id, MAX(version) AS max_version
-                    FROM post_content
-                    GROUP BY post_id
-                    ) AS t2 ON t1.post_id = t2.post_id AND t1.version = t2.max_version
+                WHERE t1.version = (
+                    SELECT MAX(version)
+                    FROM post_content AS t2
+                    WHERE t1.post_id = t2.post_id
+                )
             ) AS pc ON p.id = pc.post_id
             LEFT JOIN (
                 SELECT c.post_id, COUNT(*) AS commentCount
@@ -88,13 +88,11 @@ public interface PostRepository extends JpaRepository<PostEntity, Long> {
                     t1.content,
                     t1.post_id
                 FROM post_content AS t1
-                JOIN (
-                    SELECT
-                        post_id,
-                        MAX(version) AS max_version
-                    FROM post_content
-                    GROUP BY post_id
-                    ) AS t2 ON t1.post_id = t2.post_id AND t1.version = t2.max_version
+                WHERE t1.version = (
+                    SELECT MAX(version)
+                    FROM post_content AS t2
+                    WHERE t1.post_id = t2.post_id
+                )
             ) AS pc ON p.id = pc.post_id
             WHERE
                 p.is_active = TRUE
