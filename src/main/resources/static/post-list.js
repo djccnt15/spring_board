6f63cb343147b1c11f2acbb52a7e2fe00e2e5f02
Jay.D <djccnt15@gmail.com>
2025-01-20 22:32:59 +0900

    update templates minor fixes for code convention

--- src/main/resources/static/post-list.js
+++ src/main/resources/static/post-list.js
@@ -1,9 +1,9 @@
-const btn_search = document.getElementById("btn_search");
-const search_kw = document.getElementById("search_kw");
+const btn_search = document.getElementById("btn-search");
+const search_kw = document.getElementById("search-kw");
 
 function performSearch() {
   document.getElementById("kw").value = search_kw.value;
-  document.getElementById("size").value = document.getElementById("list_size").value;
+  document.getElementById("size").value = document.getElementById("list-size").value;
   document.getElementById("page").value = 0; // 검색 시 0 페이지부터 조회
   document.getElementById("searchForm").submit();
 }
