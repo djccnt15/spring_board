fefb6afd19014169a1900e4533da9dc12558983a
Jay.D <djccnt15@gmail.com>
2025-01-25 23:28:10 +0900

    update markdown method in CommonUtil rename method to renderMarkdown fix annotation for class to Service since Component is not explicit resolve #9

--- src/main/java/com/djccnt15/spring_board/utils/CommonUtil.java
+++ src/main/java/com/djccnt15/spring_board/utils/CommonUtil.java
@@ -3,12 +3,15 @@ package com.djccnt15.spring_board.utils;
 import org.commonmark.node.Node;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class CommonUtil {
     
-    public String markdown(String markdown) {
+    public String renderMarkdown(String markdown) {
+        if (markdown == null) {
+            return "";
+        }
         Parser parser = Parser.builder().build();
         Node document = parser.parse(markdown);
         HtmlRenderer renderer = HtmlRenderer.builder().build();
