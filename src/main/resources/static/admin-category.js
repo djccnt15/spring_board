6f05ec9eb0aa25bcdb7cfab04dde798a596c2d3e
Jay.D <djccnt15@gmail.com>
2025-01-29 16:49:53 +0900

    update admin-category page and js add `onclick="event.stopPropagation();"` for bugfix of deleting main category create basic js function to prevent hasty generalization

--- src/main/resources/static/admin-category.js
+++ src/main/resources/static/admin-category.js
@@ -1,12 +1,45 @@
-import {
-  CaseType,
-  handleFormSubmit,
-} from "./form-handler.js";
+const createForm = document.getElementsByClassName("create")
+Array.from(createForm).forEach(function(element) {
+  element.addEventListener("submit", handleFormSubmit)
+});
 
-const mainCategoryCreateForm = document.getElementById("mainCategoryCreateForm")
-mainCategoryCreateForm.addEventListener("submit", handleFormSubmit)
-mainCategoryCreateForm.setAttribute("data-case-type", CaseType.CREATE)
+const deleteForm = document.getElementsByClassName("delete");
+Array.from(deleteForm).forEach(function(element) {
+  element.addEventListener("submit", handleFormSubmit);
+});
 
-const subCategoryCreateForm = document.getElementById("subCategoryCreateForm")
-subCategoryCreateForm.addEventListener("submit", handleFormSubmit)
-subCategoryCreateForm.setAttribute("data-case-type", CaseType.CREATE)
+async function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const form = event.target;
+  const method = form.querySelector('input[name="_method"]')?.value || form.method;
+
+  switch (method.toUpperCase()) {
+    case "POST":
+      if (!confirm("생성하시겠습니까?")) return;
+      break;
+    case "PUT":
+    case "PATCH":
+      if (!confirm("수정하시겠습니까?")) return;
+      break;
+    case "DELETE":
+      if (!confirm("삭제하시겠습니까?")) return;
+      break;
+    default:
+      console.warn(`Unsupported method: ${method}`);
+  }
+  const formData = new FormData(form);
+  try {
+    const response = await fetch(form.action, {
+      method: method,
+      body: formData,
+    })
+    if (!response.ok) {
+      const data = await response.json();
+      throw new Error(`${data.message}`);
+    }
+    window.location.reload();
+  } catch (error) {
+    alert("오류가 발생했습니다:\n" + (error.message || "알 수 없는 오류"));
+  }
+}
