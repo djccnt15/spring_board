8db0472bd85242ee1ca071d67020c0bec84a2e97
Jay.D <djccnt15@gmail.com>
2025-02-23 18:39:54 +0900

    create RedisConfig, CacheService for caching data

---
+++ src/main/java/com/djccnt15/spring_board/domain/cache/service/CacheService.java
@@ -0,0 +1,37 @@
+package com.djccnt15.spring_board.domain.cache.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Service
+public class CacheService {
+    
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+    
+    /**
+     * do not use this method for not having TTL setting
+     */
+    @Deprecated
+    public void saveCache(String key, Object value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+    
+    public void saveCache(String key, Object value, long timeout, TimeUnit timeUnit) {
+        redisTemplate.opsForValue().set(key, value, timeout, timeUnit);
+    }
+    
+    public <T> T getCache(String key, Class<T> type) {
+        Object value = redisTemplate.opsForValue().get(key);
+        return type.cast(value);
+    }
+    
+    public void deleteCache(String key) {
+        redisTemplate.delete(key);
+    }
+}
