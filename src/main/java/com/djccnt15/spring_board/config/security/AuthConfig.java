73643afe5f3fe412c719ebed4f0b23b2226a82f8
Jay.D <djccnt15@gmail.com>
2025-03-03 20:09:36 +0900

    rename/mv SecretUtil to AuthConfig

--- src/main/java/com/djccnt15/spring_board/config/security/AuthConfig.java
+++ src/main/java/com/djccnt15/spring_board/config/security/AuthConfig.java
@@ -1,19 +1,17 @@
-package com.djccnt15.spring_board.utils;
+package com.djccnt15.spring_board.config.security;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
 
-@Slf4j
-@Service
-public class SecretUtil {
+@Configuration
+public class AuthConfig {
     
     /**
      * Bean for password encoding
      */
-    @Bean
+    @Bean  // make returned object as spring bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
